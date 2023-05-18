a0b0a128db06fb17ff08049620b94d6edba0df4c
Marcin <marcin.michalak34@gmail.com>
2023-05-18 20:01:44 +0200

    kontrolery do poprawy

---
+++ Backend/EFCore/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
+
+namespace Backend.EFCore
+{
+    [Table("Order")]
+    public class Order
+    {
+        [Key, Required]
+        public int ID { get; set; }
+        public int product_ID { get; set; }
+
+        public virtual product Product { get; set; }
+
+        public string name { get; set; } = string.Empty;
+
+        public string address { get; set; } = string.Empty;
+
+        public string phone { get; set; } = string.Empty;
+    }
+}

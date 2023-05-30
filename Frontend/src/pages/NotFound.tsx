e79641e3dda7ff5f92a0159f114f2d76bfa7d094
Sławomir Mendyka <thewilku@o2.pl>
2023-05-30 21:46:54 +0200

    add 404 page ✨ (#4)
    * add 404 page ✨
    
    * fix css comments
    
    * fix eslint

--- Frontend/src/pages/NotFound.tsx
+++ Frontend/src/pages/NotFound.tsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
+import styles from './NotFound.module.css';
 
-function NotFound() {
+const NotFound = () => {
   return (
-    <>
-      <h1>Not Found</h1>
-      <Link to="/">GO HOME</Link>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.message}>Strona, której szukasz, nie istnieje.</p>
+      <Link to="/" className={styles.link}>
+        Go Home
+      </Link>
+    </div>
   );
-}
+};
 
 export default NotFound;

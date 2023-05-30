812d5e918e6523a62dde8b2543a2b7b9ac0fd40d
thewilku <thewilku@o2.pl>
2023-05-30 21:39:47 +0200

    add 404 page ✨

--- Frontend/src/pages/NotFound.tsx
+++ Frontend/src/pages/NotFound.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
+import styles from './NotFound.module.css';
 
 function NotFound() {
   return (
-    <>
-      <h1>Not Found</h1>
-      <Link to="/">GO HOME</Link>
-    </>
+      <div className={styles.container}>
+          <h1 className={styles.title}>404</h1>
+          <p className={styles.message}>Strona, której szukasz, nie istnieje.</p>
+          <Link to="/" className={styles.link}>Go Home</Link>
+      </div>
   );
 }
 
